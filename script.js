fdba01277e27b4a0205a9ea651e1d7b924a967e7
alicoder123411 <sameersingla240@gmail.com>
2026-01-21 20:15:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
+	s=str.length
+	for(let i=0;i<s;i++){
+		for(let j=i+1;j<s;j++){
+			if(str[i]!==str[j]){
+				console.log(s[i])
+				break
+			}
+		}
+	}
+	
  // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+} 
